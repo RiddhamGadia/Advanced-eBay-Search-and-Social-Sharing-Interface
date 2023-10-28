0f4a1bf66e45f8b4f29ef8a2e51fb6c450b7567c
Riddham Gadia <riddham.gadia@gmail.com>
2023-10-27 17:03:44 -0700

    added j

--- Frontend/myapp/src/app/Components/results/results.component.ts
+++ Frontend/myapp/src/app/Components/results/results.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent {
+  icon: string = 'add_shopping_cart';
+
+  toogleIcon(): void {
+    if (this.icon === 'add_shopping_cart') {
+      this.icon = 'remove_shopping_cart';
+    } else {
+      this.icon = 'add_shopping_cart';
+    }
+  }
+  performTitleAction(): void {
+    // Your logic here when title is clicked
+    console.log('Title clicked!');
+  }
 
 }
